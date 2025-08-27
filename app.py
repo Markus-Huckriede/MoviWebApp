d08c01036469384254e7a884cb6265e0972dbe22
Markus Huckriede <markushuckriede@MacBook-Air-von-Markus.local>
2025-08-27 15:57:40 +0200

    data_manager.py und app.py hinzugefügt

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+import requests
+from models import Movie
+from data_manager import DataManager
+
+app = Flask(__name__)
+
+
+@app.route("/add_movie/<title>")
+def add_movie(title):
+    API_KEY = ""
+    url = f"http://www.omdbapi.com/?t={title}&apikey={API_KEY}"
+    response = requests.get(url).json()
+    '''
+    Error handling if movie does not exist
+    '''
+    if response.get("Response") == "False":
+        return f"Movie '{title}' not found in OMDb.", 404
+
+    '''
+    creating movie object
+    '''
+    movie = Movie(
+        name=response["Title"],
+        director=response["Director"],
+        year=response["Year"],
+        poster_url=response["Poster"],
+    )
+    '''
+    save movie
+    '''
+    dm = DataManager()
+    dm.add_movie(movie)
+    return f"{movie.name} successfully added!"
---
+++ data_manager.py
@@ -0,0 +1,34 @@
+from models import db, User, Movie
+
+
+class DataManager:
+    def __init__(self):
+        pass
+
+    def create_user(self, name):
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+
+    def get_users(self):
+        return User.query.all()
+
+    def get_movies(self, user_id):
+        return Movie.query.filter_by(user_id=user_id).all()
+
+    def add_movie(self, movie):
+        new_movie = Movie(
+            name=name,
+            director=director,
+            year=year,
+            poster_url=poster_url,
+            user_id=user_id
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+
+    def update_movie(self, movie_id, new_title):
+        pass
+
+    def delete_movie(self, movie_id):
+        pass
