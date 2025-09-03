5b8aadce7b26d763f5bb3147ab4f4ec320dceac5
Markus Huckriede <markushuckriede@d56d22dc-c7b2-4427-8da2-49b80e2ed967.fritz.box>
2025-09-03 13:16:08 +0200

    routes defined

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template, url_for, redirect
 import requests
 from models import db, Movie
 from data_manager import DataManager
@@ -15,7 +15,8 @@ data_manager = DataManager() # Create an object of your DataManager class
 
 @app.route('/')
 def home():
-    return "Welcome to MoviWeb App!"
+    users = data_manager.get_users()
+    return render_template('home.html', users=users)
 
 
 @app.route("/add_movie/<title>")
@@ -37,13 +38,21 @@ def add_movie(title):
         director=response["Director"],
         year=response["Year"],
         poster_url=response["Poster"],
+        user_id = 1
     )
     '''
     save movie
     '''
-    dm = DataManager()
-    dm.add_movie(movie)
+    data_manager.add_movie(movie)
     return f"{movie.name} successfully added!"
+
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    name = request.form.get("name")
+    if name:
+        data_manager.create_user(name)
+    return redirect(url_for('home'))
 
 
 @app.route('/users')
@@ -52,10 +61,39 @@ def list_users():
     return str(users)  # Temporarily returning users as a string
 
 
-"""
+@app.route('/movies/<int:user_id>')
+def get_movies(user_id):
+    movies = data_manager.get_movies(user_id)
+    return render_template("movies.html", movies=movies)
+
+
+@app.route('/users/<int:user_id>/movies', methods=['GET'])
+def list_movies(user_id):
+    movies = data_manager.get_movies(user_id)
+    return render_template('movies.html', movies=movies, user_id=user_id)
+
+
+@app.route('/users/<int:user_id>/movies/<int:movie_id>/delete', methods=['POST'])
+def delete_movie(user_id, movie_id):
+    msg = data_manager.delete_movie(movie_id)
+    return redirect(url_for('list_movies', user_id=user_id))
+
+
+@app.route('/users/<int:user_id>/movies', methods=['GET', 'POST'])
+def user_movies(user_id):
+    if request.method == "GET":
+        movies = data_manager.get_movies(user_id)
+        return render_template("movies.html", movies=movies, user_id=user_id)
+    elif request.method == "POST":
+        # add new movie
+        title = request.form.get("title")
+        # OMDb, create movie
+        data_manager.add_movie(movie)
+    return redirect(url_for("user_movies", user_id=user_id))
+
+
 if __name__ == '__main__':
   with app.app_context():
     db.create_all()
 
   app.run()
-"""
--- data_manager.py
+++ data_manager.py
@@ -5,27 +5,26 @@ class DataManager:
     def __init__(self):
         pass
 
+
     def create_user(self, name):
         new_user = User(name=name)
         db.session.add(new_user)
         db.session.commit()
 
+
     def get_users(self):
         return User.query.all()
+
 
     def get_movies(self, user_id):
         return Movie.query.filter_by(user_id=user_id).all()
 
+
     def add_movie(self, movie):
-        new_movie = Movie(
-            name=name,
-            director=director,
-            year=year,
-            poster_url=poster_url,
-            user_id=user_id
-        )
-        db.session.add(new_movie)
+        db.session.add(movie)
         db.session.commit()
+        return movie
+
 
     def update_movie(self, movie_id, new_title):
         movie = Movie.query.get(movie_id)
@@ -35,5 +34,12 @@ class DataManager:
             return f"Movie '{new_title}' updated"
         return f"Movie with ID {movie_id} not found"
 
+
     def delete_movie(self, movie_id):
-        pass
+        movie = Movie.query.get(movie_id)
+        if movie:
+            db.session.delete(movie)
+            db.session.commit()
+            return f"Movie '{movie.name}' deleted"
+        return f"Movie with ID {movie_id} not found"
+
